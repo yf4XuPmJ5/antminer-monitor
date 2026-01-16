61abb920b4ec2487073b90d5b4cbdfc1cb731571
iuukai <raitiolarita@outlook.com>
2026-01-16 17:27:02 +0100

    update

---
+++ lib/util_hashrate.py
@@ -0,0 +1,22 @@
+# Update from one unit to the next if the value is greater than 1024.
+# e.g. update_unit_and_value(1024, "GH/s") => (1, "TH/s")
+
+
+def update_unit_and_value(value, unit):
+    while value > 1024:
+        value = value / 1024.0
+        if unit == 'MH/s':
+            unit = 'GH/s'
+        elif unit == 'GH/s':
+            unit = 'TH/s'
+        elif unit == 'TH/s':
+            unit = 'PH/s'
+        elif unit == 'PH/s':
+            unit = 'EH/s'
+        elif unit == 'KSol/s':
+            unit = 'MSol/s'
+        elif unit == 'MSol/s':
+            unit = 'GSol/s'
+        else:
+            assert False, "Unsupported unit: {}".format(unit)
+    return (value, unit)
